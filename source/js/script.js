7dd7acfa07e49e6e2b0880c0449e1cf3c93964d4
Антон Куклин <homyak91@inbox.ru>
2021-10-31 16:35:02 +0400

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,16 @@
 // Переменные
 
+let catalogSection = document.querySelector(".catalog-section")
 let catalog = document.querySelector(".catalog");
 let cards = [];
 let maxShowCardsCount = 6;
 let eroticCatalog = document.querySelector(".catalog--erotic");
 let dailyButton = document.querySelector("#daily");
 let eroticButton = document.querySelector("#erotic");
+let dailyHeaderButton = document.querySelector("#header-daily");
+let headerEroticButton = document.querySelector("#header-erotic");
+let target = document.querySelector("#catalog");
+let popup = document.querySelector(".popup");
 
 // Получение данных о товарах
 
@@ -32,12 +37,6 @@ function loadData() {
 
 loadData();
 
-// Отрисовка карточек товара
-
-function render() {
-  catalog.innerHTML = "";
-}
-
 // Cоздание карточки товара
 
 function createCard(good) {
@@ -51,15 +50,15 @@ function createCard(good) {
   card.querySelector(".card__title").textContent = `Портупея "${good.name}"`;
 
   for (let i = 0; i < sizesItems.length; i++) {
-    sizesItems[i].dataset["value"] = good.sizes[i];
-    if (sizesItems[i].dataset["value"] === "false") {
+    sizesItems[i].dataset["instock"] = good.sizes[i];
+    if (sizesItems[i].dataset["instock"] === "false") {
       sizesItems[i].classList.add("sizes-list__item--inactive");
     }
   }
 
   for (let j = 0; j < colorItems.length; j++) {
-    colorItems[j].dataset["value"] = good.colors[j];
-    if (colorItems[j].dataset["value"] === "false") {
+    colorItems[j].dataset["instock"] = good.colors[j];
+    if (colorItems[j].dataset["instock"] === "false") {
       colorItems[j].classList.add("colors-list__item--inactive");
     }
   }
@@ -75,9 +74,46 @@ function createCard(good) {
   if (good.price) {
     card.querySelector(".prices__price").textContent = good.price;
   }
+  // Функция вызова поп-апа
+  function makeOrder() {
+    if (catalog) {
+      let cardButtons = card.querySelectorAll("#card-button");
+      cardButtons.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+         popup.classList.remove("popup--hidden")
+        //  Отрисовка значении попапа
+         let popupTitle = document.querySelector(".popup__title");
+         let popupImage = document.querySelector(".popup__image")
+         let popupPrice = popup.querySelector(".prices__price")
+         let popupOldPrice = popup.querySelector(".prices__old-price")
+         popupTitle.textContent = `Портупея "${good.name}"`;
+         popupImage.src = good.img;
+         popupPrice.textContent = good.price;
+         if (popupOldPrice) {
+           popupOldPrice.textContent = good.oldPrice;
+         } else {
+          popupOldPrice.classList.add("prices__old-price--hidden");
+         }
+        });
+      });
+    }
+  }
+  makeOrder();
 
   return card;
 }
+
+if (popup) {
+  function closePopup() {
+    let popupButton = popup.querySelector(".popup__button");
+    popupButton.addEventListener("click", function() {
+      popup.classList.add("popup--hidden")
+    })
+  }
+}
+closePopup();
+
+// Отрисовка карточки товара
 
 function renderCard(cards) {
   let cardsToRender = cards.slice(0, Math.min(cards.length, maxShowCardsCount));
@@ -86,6 +122,8 @@ function renderCard(cards) {
     catalog.append(cardElement);
   }
 }
+
+// Функция заказа
 
 // Переключение каталогов
 
@@ -115,3 +153,38 @@ if (catalog && eroticCatalog) {
 }
 
 changeCatalogues();
+
+// Плавный скролл при нажатии на кнопки каталога в интро-блоке
+
+function addSmoothScroll() {
+  let moveTo = new MoveTo({
+    duration: 1200,
+  });
+  if (dailyHeaderButton) {
+    dailyHeaderButton.addEventListener("click", function () {
+      moveTo.move(target);
+    });
+  }
+  if (headerEroticButton) {
+    headerEroticButton.addEventListener("click", function () {
+      moveTo.move(target);
+      dailyButton.classList.remove("catalog-section__button--active");
+      eroticButton.classList.add("catalog-section__button--active");
+      catalog.classList.add("catalog--inactive");
+      eroticCatalog.classList.remove("catalog--inactive");
+    });
+  }
+}
+
+addSmoothScroll();
+// let triggers = menuLinks;
+
+// triggers.forEach(function (trigger) {
+//   moveTo.registerTrigger(trigger);
+// });
+
+// Кастомный селект
+
+if (document.querySelector("#select-1")) {
+  const select1 = new CustomSelect("#select-1");
+}
